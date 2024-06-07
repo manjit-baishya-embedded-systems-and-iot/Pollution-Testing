548a789d35a8c6252f62dda55a277c0f8f3997b4
Manjit Baishya <manjitbaishya100@gmail.com>
2024-06-07 22:17:38 +0530

    code updated

--- air_quality_sensor/air_quality_sensor.ino
+++ air_quality_sensor/air_quality_sensor.ino
@@ -1,25 +1,17 @@
-// define the pin for the ZP07-MP503 sensor
-const int terminal_A = A0;  // terminal A is connected to analog pin A0
-const int terminal_B = A1;  // terminal B is connected to analog pin A0
+const int sensorPin = A0;  // assuming the sensor is connected to analog pin A0
 
 void setup() {
-  // initialize the serial communication
-  Serial.begin(9600);
-  // configure the sensor pin as input
-  pinMode(terminal_A, INPUT);
-  pinMode(terminal_B, INPUT);
+
+  Serial.begin(15200);
+
+  pinMode(sensorPin, INPUT);
 }
 
 void loop() {
-  // read the value from the sensor
-  int value_A = analogRead(terminal_A);
-  int value_B = analogRead(terminal_B);
-  
-  // print the sensor value to the serial monitor
-  Serial.print("Sensor Value: ");
-  Serial.println(value_A);
-  Serial.println(value_B);
-  
-  // wait for 1 second before the next reading
+
+  int sensorValue = analogRead(sensorPin);
+
+  Serial.println(sensorValue);
+
   delay(1000);
 }
